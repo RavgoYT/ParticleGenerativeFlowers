788a40201d2d6d44f45b1a35a05a8ab2a2c2b5ac
RavgoYT <88573982+RavgoYT@users.noreply.github.com>
2024-02-14 14:54:31 -0800

    Update shapes.js

--- shapes.js
+++ shapes.js
@@ -153,7 +153,7 @@ function cutText(theText, x, y, t_Size, s_Weight, f_A, s_A){
   overlay.translate(0, 0);
   overlay.rotate(-HALF_PI);
   overlay.textSize(80);
-  overlay.text(theText, 0 - 460, 0 + 1200);
+  overlay.text(theText, 0 - 520, 0 + 1200);
   overlay.pop();
   overlay.noErase();
 }
